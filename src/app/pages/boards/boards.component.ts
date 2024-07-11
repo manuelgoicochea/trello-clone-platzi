a732965a966820aa39c9632e7063a7ac200480b1
manuelgoicochea <manuelgmdex@gmail.com>
2024-07-11 18:02:55 -0500

    se agregaron cambios en board

--- src/app/pages/boards/boards.component.ts
+++ src/app/pages/boards/boards.component.ts
@@ -1,10 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import {faTrello} from '@fortawesome/free-brands-svg-icons';
+
+import {faBox,faWaveSquare,faClock} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-boards',
   templateUrl: './boards.component.html'
 })
 export class BoardsComponent implements OnInit {
+  faTrello = faTrello;
+  faBox = faBox;
+  faWaveSquare = faWaveSquare;
+  faClock = faClock;
 
   constructor() { }
 
