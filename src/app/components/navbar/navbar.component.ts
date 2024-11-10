849c5402cf963156e745adb1bc0dbd303d25bb99
mgoicochea <manuelgmdex@gmail.com>
2024-11-10 06:51:21 -0500

    enhancement(tabla and login )[add filters and better UI]

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import{ faBell, faInfoCircle} from '@fortawesome/free-solid-svg-icons';
+import{ faBell, faClose, faInfoCircle} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-navbar',
@@ -9,7 +9,7 @@ export class NavbarComponent implements OnInit {
 
   faBell = faBell;
   faInfoCircle = faInfoCircle;
-  
+  faClose = faClose;
 
   isOpen = false;
   isOpenBody = false;
--- src/app/pages/table/table.component.ts
+++ src/app/pages/table/table.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import {HttpClient} from '@angular/common/http';
 import { Product } from '../../models/product.model';
+import { DataSourceProduct } from './data-source';
+import { debounceTime } from 'rxjs';
 
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html'
 })
 export class TableComponent implements OnInit {
-  products : Product[] = [];
-  columns : string[] = ['#No','Name','price','cover'];
+  dataSource = new DataSourceProduct();
+  columns : string[] = ['#No','Name','price','cover','actions'];
   total = 0;
+  input = new FormControl('',{ nonNullable: true});
 
   constructor(
     private http:HttpClient
@@ -20,12 +24,25 @@ export class TableComponent implements OnInit {
    
     this.http.get<Product[]>('https://api.escuelajs.co/api/v1/products')
     .subscribe(data => {
-      this.products = data;
-      this.total = this.products
-      .map(item => item.price)
-      .reduce((price,total)=>price + total,0);
+      this.dataSource.init(data);
+      this.total = this.dataSource.getTotal();
     })
+
+    this.input.valueChanges
+    .pipe(
+      debounceTime(300)
+    )
+    .subscribe(value =>{
+      this.dataSource.find(value);
+      //console.log(value);
+    });
      
+  }
+
+
+
+  update(product : Product){
+    this.dataSource.update(product.id,{price: 20});
   }
 
 }
